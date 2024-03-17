45f950f1037a04f3e439e5c6c3ab2872790450f4
Micah van de Merwe <micahvdm@gmail.com>
2024-03-17 12:19:41 +0200

    incorrect folder naming

--- stage2/05-pistomp/files/first-run.sh
+++ stage2/05-pistomp/files/first-run.sh
@@ -14,5 +14,14 @@ if [ ! -e ~/.config/pistomp-first-run ]; then
 	sudo chown pistomp:pistomp /etc/authbind/byport/80
 	/home/pistomp/pi-stomp/setup/pi-stomp-tweaks/modify_version.sh 2.0
 	/home/pistomp/pi-stomp/util/change-audio-card.sh iqaudio-codec
+	ln -sf /usr/lib/systemd/system/browsepy.service /etc/systemd/system/multi-user.target.wants
+	ln -sf /usr/lib/systemd/system/jack.service /etc/systemd/system/multi-user.target.wants
+	ln -sf /usr/lib/systemd/system/mod-host.service /etc/systemd/system/multi-user.target.wants
+	ln -sf /usr/lib/systemd/system/mod-ui.service /etc/systemd/system/multi-user.target.wants
+	ln -sf /usr/lib/systemd/system/mod-amidithru.service /etc/systemd/system/multi-user.target.wants
+	ln -sf /usr/lib/systemd/system/mod-touchosc2midi.service /etc/systemd/system/multi-user.target.wants
+	ln -sf /usr/lib/systemd/system/mod-midi-merger.service /etc/systemd/system/multi-user.target.wants
+	ln -sf /usr/lib/systemd/system/mod-midi-merger-broadcaster.service /etc/systemd/system/multi-user.target.wants
+	ln -sf /usr/lib/systemd/system/mod-ala-pi-stomp.service /etc/systemd/system/multi-user.target.wants
 	clear && cat /run/motd.dynamic
 fi
