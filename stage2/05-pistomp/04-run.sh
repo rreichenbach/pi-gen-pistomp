7ebaa97d4a3d6c71e6804640a33461615adae964
DivideCo <micahvdm@gmail.com>
2024-04-27 22:04:22 +0200

    Update 04-run.sh

--- stage2/05-pistomp/04-run.sh
+++ stage2/05-pistomp/04-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-install -m 666 files/first-run.sh ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
+install -m 666 files/firstboot.sh ${ROOTFS_DIR}/boot/
 install -m 666 files/display-pistomp-logo ${ROOTFS_DIR}/etc/update-motd.d/
