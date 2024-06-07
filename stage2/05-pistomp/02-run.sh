ee6de49f65df8cb315a83b70b07925b8b8d9f1dc
DivideCo <micahvdm@gmail.com>
2024-06-07 15:14:27 +0200

    wrong place

--- stage2/05-pistomp/02-run.sh
+++ stage2/05-pistomp/02-run.sh
@@ -68,17 +68,5 @@ cd lilv-0.24.12
 ./waf install
 cd ..
 
-chmod +x /etc/update-motd.d/display-pistomp-logo
-chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
-chmod +x /etc/jackdrc
-chown jack:jack /etc/jackdrc
-chmod 500 /etc/authbind/byport/80
-chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} /etc/authbind/byport/80
-chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /usr/lib/pistomp-wifi
-chmod +x -R /usr/lib/pistomp-wifi
-rm /etc/profile.d/bash_completion.sh
-/home/${FIRST_USER_NAME}/pi-stomp/util/change-audio-card.sh iqaudio-codec
-/home/${FIRST_USER_NAME}/pi-stomp/setup/pi-stomp-tweaks/modify_version.sh 3.0
-
 EOF
 
