b6127d13414e43e95a90ca6f99feeea22d153869
DivideCo <micahvdm@gmail.com>
2024-06-07 15:16:15 +0200

    Correct placement

--- stage2/05-pistomp/04-run.sh
+++ stage2/05-pistomp/04-run.sh
@@ -1,5 +1,21 @@
-#!/bin/bash -e
+#!/bin/bash
 
 install -m 666 files/firstboot.sh ${ROOTFS_DIR}/boot/firmware/
 install -m 666 files/reboot.sh ${ROOTFS_DIR}/boot/firmware/
 install -m 666 files/display-pistomp-logo ${ROOTFS_DIR}/etc/update-motd.d/
+
+on_chroot << EOF
+
+chmod +x /etc/update-motd.d/display-pistomp-logo
+chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
+chmod +x /etc/jackdrc
+chown jack:jack /etc/jackdrc
+chmod 500 /etc/authbind/byport/80
+chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} /etc/authbind/byport/80
+chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /usr/lib/pistomp-wifi
+chmod +x -R /usr/lib/pistomp-wifi
+rm /etc/profile.d/bash_completion.sh
+/home/${FIRST_USER_NAME}/pi-stomp/util/change-audio-card.sh iqaudio-codec
+/home/${FIRST_USER_NAME}/pi-stomp/setup/pi-stomp-tweaks/modify_version.sh 3.0
+
+EOF
