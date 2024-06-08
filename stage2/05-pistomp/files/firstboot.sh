f749e5fa8b6de8167443afdf6dd630d4ac30dc6b
DivideCo <micahvdm@gmail.com>
2024-06-08 23:57:09 +0200

    Update firstboot.sh

--- stage2/05-pistomp/files/firstboot.sh
+++ stage2/05-pistomp/files/firstboot.sh
@@ -1,33 +1,9 @@
 #!/bin/sh
 
-set -x
-set -e
-
-sudo chmod +x /etc/update-motd.d/display-pistomp-logo
-sudo chown -R pistomp:pistomp /home/pistomp
-sudo chmod +x /etc/jackdrc
-sudo chown jack:jack /etc/jackdrc
-sudo chmod 500 /etc/authbind/byport/80
-sudo chown pistomp:pistomp /etc/authbind/byport/80
-sudo chown -R pistomp:pistomp /usr/lib/pistomp-wifi
-sudo chmod +x -R /usr/lib/pistomp-wifi
 /home/pistomp/pi-stomp/util/change-audio-card.sh iqaudio-codec
-sudo ln -sf /usr/lib/systemd/system/browsepy.service /etc/systemd/system/multi-user.target.wants
-sudo ln -sf /usr/lib/systemd/system/jack.service /etc/systemd/system/multi-user.target.wants
-sudo ln -sf /usr/lib/systemd/system/mod-host.service /etc/systemd/system/multi-user.target.wants
-sudo ln -sf /usr/lib/systemd/system/mod-ui.service /etc/systemd/system/multi-user.target.wants
-sudo ln -sf /usr/lib/systemd/system/mod-amidithru.service /etc/systemd/system/multi-user.target.wants
-sudo ln -sf /usr/lib/systemd/system/mod-touchosc2midi.service /etc/systemd/system/multi-user.target.wants
-sudo ln -sf /usr/lib/systemd/system/mod-midi-merger.service /etc/systemd/system/multi-user.target.wants
-sudo ln -sf /usr/lib/systemd/system/mod-midi-merger-broadcaster.service /etc/systemd/system/multi-user.target.wants
-sudo ln -sf /usr/lib/systemd/system/mod-ala-pi-stomp.service /etc/systemd/system/multi-user.target.wants
-#sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
-clear && cat /run/motd.dynamic
-sudo rm /etc/profile.d/bash_completion.sh
-/home/pistomp/pi-stomp/setup/pi-stomp-tweaks/modify_version.sh 2.0
+/home/pistomp/pi-stomp/setup/pi-stomp-tweaks/modify_version.sh 3.0
 sudo chown -R pistomp:pistomp /home/pistomp/data
 cd /home/pistomp/pi-stomp
 git pull
-cd /boot/firmware
 sudo mv "$0" firstboot.done
 sudo reboot
