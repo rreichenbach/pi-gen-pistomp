f681f40d18a187acd900da97e9c6a2b2ea0b1cb2
DivideCo <micahvdm@gmail.com>
2024-06-08 22:51:45 +0200

    Update 04-run.sh

--- stage2/05-pistomp/04-run.sh
+++ stage2/05-pistomp/04-run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-install -m 666 files/firstboot.sh ${ROOTFS_DIR}/boot/firmware/
-install -m 666 files/reboot.sh ${ROOTFS_DIR}/boot/firmware/
 install -m 666 files/display-pistomp-logo ${ROOTFS_DIR}/etc/update-motd.d/
 
 on_chroot << EOF
@@ -15,7 +13,6 @@ chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} /etc/authbind/byport/80
 chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /usr/lib/pistomp-wifi
 chmod +x -R /usr/lib/pistomp-wifi
 rm /etc/profile.d/bash_completion.sh
-# /home/${FIRST_USER_NAME}/pi-stomp/util/change-audio-card.sh iqaudio-codec
 /home/${FIRST_USER_NAME}/pi-stomp/setup/pi-stomp-tweaks/modify_version.sh 3.0
 
 EOF
