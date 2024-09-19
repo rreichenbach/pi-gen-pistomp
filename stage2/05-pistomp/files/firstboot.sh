2c929f3a0ed981fe2025416cc3001eeb769ebb05
randallreichenbach <randaji@gmail.com>
2024-09-19 11:07:17 -0700

    MVP: fixes to allow booting and MOD starting, still need to fix hotspot and default_config

--- stage2/05-pistomp/files/firstboot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-/home/pistomp/pi-stomp/util/change-audio-card.sh iqaudio-codec
-/home/pistomp/pi-stomp/setup/pi-stomp-tweaks/modify_version.sh 3.0
-sudo chown -R pistomp:pistomp /home/pistomp/
-cd /home/pistomp/pi-stomp
-git pull
-sudo mv "$0" firstboot.done
-sudo reboot
